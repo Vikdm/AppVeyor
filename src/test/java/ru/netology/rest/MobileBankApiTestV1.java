6fe0d34e1e746d27cbf41cebb364ea2dc697168b
vikdm <viktorov@my.com>
2022-07-22 18:58:16 +0300

    падение по несоотвествию ID

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,13 +19,13 @@ class MobileBankApiTestV1 {
           .statusCode(200);
     }
 
-    @Test
-    void shouldReturnDemoAccounts1() {
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(201);
-    }
+//    @Test
+//    void shouldReturnDemoAccounts1() {
+//        given()
+//                .baseUri("http://localhost:9999/api/v1")
+//                .when()
+//                .get("/demo/accounts")
+//                .then()
+//                .statusCode(201);
+//    }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -26,5 +27,21 @@ class MobileBankApiTestV3 {
           .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
       ;
+    }
+    @Test
+    void shouldReturnDemoAccounts1() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200)
+
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[2].id", equalTo("12"))
+        ;
     }
 }
