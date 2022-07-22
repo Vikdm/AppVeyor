3bb18ddc3650bfda1d8a884a42d1283d6d99d68f
vikdm <viktorov@my.com>
2022-07-22 21:37:08 +0300

    RUR тест падения

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -11,22 +11,22 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
     }
 //    @Test
 //    void shouldReturnDemoAccounts1() {
@@ -46,7 +46,7 @@ class MobileBankApiTestV3 {
 //    }
 
     @Test
-    void validCurrency (){
+    void validCurrency() {
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 .when()
@@ -56,7 +56,20 @@ class MobileBankApiTestV3 {
 
                 .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
-              ;
+        ;
 
+    }
+
+    @Test
+    void validCurrency1() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .contentType(ContentType.JSON)
+
+                .body("[0].currency", equalTo("RUR"))
+        ;
     }
 }
